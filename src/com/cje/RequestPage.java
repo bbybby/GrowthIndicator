fe34ae04907bb2bafdcb23010834146ef730e2e3
Jieun Choi <grace2171@gmail.com>
2016-06-09 00:58:06 -0700

    Initial Commit

---
+++ src/com/cje/RequestPage.java
@@ -0,0 +1,17 @@
+package com.cje;
+
+import javax.swing.*;
+
+/**
+ * Created by bbybby on 6/8/2016.
+ */
+public class RequestPage {
+    private JTextField titleTextField;
+
+    private JPanel panel1;
+    private JPanel inputPanel;
+    private JRadioButton maleRdBtn;
+    private JRadioButton femaleRdBtn;
+    private JComboBox birthYearCBox;
+    private JComboBox birthMonthCBox;
+}
