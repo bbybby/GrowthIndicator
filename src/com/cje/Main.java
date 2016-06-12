c8da5e73b6ce24e10069a3e63b0f591beec6c5f6
Jieun Choi <grace2171@gmail.com>
2016-06-12 00:04:56 -0700

    Finished UI

--- src/com/cje/Main.java
+++ src/com/cje/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         RequestPage mainFrm = new RequestPage();
-        mainFrm.setSize(600, 800);
+        mainFrm.setSize(700, 600);
         mainFrm.setVisible(true);
     }
 }
--- src/com/cje/RequestPage.java
+++ src/com/cje/RequestPage.java
@@ -1,6 +1,11 @@
 package com.cje;
 
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import javax.swing.text.NumberFormatter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 
 /**
  * Created by bbybby on 6/8/2016.
@@ -30,5 +35,22 @@ public class RequestPage extends JFrame{
         pack();
         setLocationRelativeTo(null);
         //setVisible(true);
+        viewBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello!"+ birthWeightTxt.getValue());
+            }
+        });
+
+    }
+
+
+    private void createUIComponents() {
+        //NumberFormat fmt = NumberFormat.getNumberInstance();
+        MaskFormatter fmt = new MaskFormatter();
+        fmt.setValidCharacters("0123456789.");
+        //fmt.setMaximumFractionDigits(2);
+        birthWeightTxt = new JFormattedTextField(fmt);
+        birthWeightTxt.setVisible(true);
     }
 }
